b1f09ba2dfac2c299395dc030ed1ec87a58636db
Ricky Reusser <rsreusser@gmail.com>
2014-09-16 16:41:12 -0400

    Commit today's changes as we wire up articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,16 @@
 class ArticlesController < ApplicationController
   def show
+  	@article = Article.find(params[:id])
   end
 
   def new
   	@article = Article.new
+  end
+
+  def create
+  	@article = Article.create( article_params )
+
+  	redirect_to article_path(@article)
   end
 
   def index
@@ -11,4 +18,9 @@ class ArticlesController < ApplicationController
 
   def edit
   end
+
+  private
+  	def article_params
+  		params.require(:article).permit(:content,:title,:author,:category)
+  	end
 end
